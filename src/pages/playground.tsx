1a2242a12f55ac4e633ad1e4f1b15995dd685457
Willin Wang <willin@willin.org>
2022-02-15 02:41:58 +0800

    style: :art: swagger style

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -28,10 +28,8 @@ const ApiDoc: NextPage<{
       <main>
         <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
           {/* Replace with your content */}
-          <div className='px-4 py-6 sm:px-0'>
-            <div className='border-4 border-dashed border-gray-200 rounded-lg'>
-              <SwaggerUI spec={spec} />
-            </div>
+          <div className='px-4 py-6 sm:px-0 bg-white rounded-lg'>
+            <SwaggerUI spec={spec} />
           </div>
           {/* /End replace */}
         </div>
